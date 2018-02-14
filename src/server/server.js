295eb0ff40adef14413d02ca310502bb09be53ff
unknown <=>
2018-02-14 17:34:22 -0600

    Prueba

--- src/server/server.js
+++ src/server/server.js
@@ -15,9 +15,19 @@ app.get('/users/:id', (req, res) => {
   res.json({ name: users[req.params.id] })
 })
 
+/*
+app.get('/anthony', (req, res) => {  
+  res.json({ name: 'Anthony Rojas Alfaro'})
+})*/
+
 app.get('/companies', (req, res) => {
   // parametros query. Ejemplo /companies?hola=true
   res.json({ name: req.query.name })
+})
+
+app.get('/anthony/:apellido1/:apellido2', (req, res) => {
+  // parametros query. Ejemplo /companies?hola=true
+  res.json({ name: 'Anthony', apelido1: req.params.apellido1, apelido2: req.params.apellido2  })
 })
 
 app.listen(3005, () => {
